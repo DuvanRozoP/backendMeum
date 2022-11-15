47c0224a6e1c289572b6e3ffa48f0f4e3ce0dd46
Duvanrozop <rozoparraduvan@gmail.com>
2022-11-15 15:12:25 -0500

    update config.server.js

--- config.server.js
+++ config.server.js
@@ -1,21 +1,21 @@
 const user = 'duvanAdmin'
 const password = 'mluY4orY1BaB7z2e'
 const uri = `mongodb+srv://${user}:${password}@cluster0.bvxrtoj.mongodb.net/?retryWrites=true&w=majority`
-
+/*
 const config = {
     url: uri,
     port: process.env.PORT || 3000,
     host: process.env.HOST || 'http://localhost:',
     publicRoute: process.env.PUBLIC_ROUTE || '/static'
 }
+*/
 
-
-// const config = {
-//     url: uri,
-//     port: process.env.PORT || 3000,
-//     host: process.env.HOST || 'https://richardardilaayala.herokuapp.com:',
-//     publicRoute: process.env.PUBLIC_ROUTE || '/static'
-// }
+const config = {
+    url: uri,
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || 'https://richardardilaayala.herokuapp.com',
+    publicRoute: process.env.PUBLIC_ROUTE || '/static'
+}
 
 
 module.exports = config
